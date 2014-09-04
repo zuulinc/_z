28bb6d79f43919a242e84a7f5c13df95e3c6b1b7
Keith Foster <keith@zuul.ca>
2014-09-04 11:04:02 -0400

    adding the "updated" string

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -68,7 +68,7 @@ if ( ! function_exists( '_z_posted_on' ) ) :
 function _z_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time> <time class="updated" datetime="%3$s">Updated on: %4$s</time>';
 	}
 
 	$time_string = sprintf( $time_string,
